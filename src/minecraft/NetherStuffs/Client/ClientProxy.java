6329ddac1fbbd81c906216f6c5a226e7c92f79d6
BStramke <bstramke@gmail.com>
2012-10-24 09:08:56 +0200

    Added the ClientProxy to Github

---
+++ src/minecraft/NetherStuffs/Client/ClientProxy.java
@@ -0,0 +1,11 @@
+package NetherStuffs.Client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import NetherStuffs.Common.CommonProxyNetherStuffs;
+
+public class ClientProxy extends CommonProxyNetherStuffs{
+	@Override
+	public void registerRenderThings(){
+		MinecraftForgeClient.preloadTexture("/blocks.png");
+	}
+}
