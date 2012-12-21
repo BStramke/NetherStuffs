bffa4de97c0f6547ab864ebf9e72b4a28c19d4ab
BStramke <bstramke@gmail.com>
2012-12-21 17:11:24 +0100

    1.4.6 stuff

---
+++ build_netherstuffs.bat
@@ -0,0 +1,29 @@
+@echo off
+mkdir reobf\minecraft\NetherStuffsBase
+mkdir reobf\minecraft\NetherStuffsBase\buildcraft
+mkdir reobf\minecraft\NetherStuffsBase\buildcraft\api
+mkdir reobf\minecraft\NetherStuffsBase\buildcraft\api\inventory
+mkdir reobf\minecraft\NetherStuffsCoreBase
+move reobf\minecraft\NetherStuffs reobf\minecraft\NetherStuffsBase
+move reobf\minecraft\NetherStuffsCore reobf\minecraft\NetherStuffsCoreBase
+copy reobf\minecraft\buildcraft\api\inventory\* reobf\minecraft\NetherStuffsBase\buildcraft\api\inventory
+copy NetherStuffs_Resources\mcmod.info reobf\minecraft\NetherStuffsBase\mcmod.info
+copy NetherStuffs_Resources\textures\* reobf\minecraft\NetherStuffsBase
+copy NetherStuffs_Resources\gui\* reobf\minecraft\NetherStuffsBase
+mkdir reobf\minecraft\NetherStuffsCoreBase\META-INF
+copy src\common\netherstuffs_at.cfg reobf\minecraft\NetherStuffsCoreBase\netherstuffs_at.cfg
+copy src\common\NetherStuffsCore\META-INF\MANIFEST.MF reobf\minecraft\NetherStuffsCoreBase\META-INF\MANIFEST.MF
+rem copy Custom Class Files
+rem BlockBreakable
+copy reobf\minecraft\akf.class reobf\minecraft\NetherStuffsCoreBase\akf.class 
+rem BlockPane
+copy reobf\minecraft\ami.class reobf\minecraft\NetherStuffsCoreBase\ami.class 
+cd reobf\minecraft\NetherStuffsBase
+"C:\Program Files\7-Zip\7z.exe" a -tzip mod_NetherStuffs.zip *
+cd..
+cd NetherStuffsCoreBase
+"C:\Program Files\7-Zip\7z.exe" a -tzip mod_NetherStuffsCore.zip *
+cd ..
+move NetherStuffsBase\mod_NetherStuffs.zip mod_NetherStuffs.zip
+move NetherStuffsCoreBase\mod_NetherStuffsCore.zip mod_NetherStuffsCore.jar
+pause
