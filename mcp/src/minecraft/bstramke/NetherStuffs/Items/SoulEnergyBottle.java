de96d1a2bb13de01ad4bd9d02c2a409fd8f40a88
BStramke <bstramke@gmail.com>
2013-03-22 12:18:42 +0100

    Icons for partially filled soulenergy bottles added

--- mcp/src/minecraft/bstramke/NetherStuffs/Items/SoulEnergyBottle.java
+++ mcp/src/minecraft/bstramke/NetherStuffs/Items/SoulEnergyBottle.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.Icon;
 import bstramke.NetherStuffs.Common.CommonProxy;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -20,7 +21,8 @@ public class SoulEnergyBottle extends Item {
 	public static final int medium = 1;
 	public static final int large = 2;
 	public static final int largeFilled = 3;
-
+	private Icon icoFillings[];
+	
 	protected SoulEnergyBottle(int par1) {
 		super(par1);
 		this.setMaxStackSize(1);
@@ -35,13 +37,34 @@ public class SoulEnergyBottle extends Item {
 	}
 
 	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIconFromDamage(int damage) {
+		if(damage <= large)
+			return iconIndex;
+		if(damage == largeFilled)
+			return icoFillings[7];
+		
+		if(damage>=4 && damage<=11)
+			return icoFillings[damage-4];
+		if(damage>=12 && damage<=19)
+			return icoFillings[damage-12];
+		if(damage>=20 && damage<=27)
+			return icoFillings[damage-20];
+		
+		return iconIndex;
+	}
+	
+	@Override
 	public void func_94581_a(IconRegister iconRegister)
 	{
+		icoFillings = new Icon[8];
 		iconIndex = iconRegister.func_94245_a(CommonProxy.getIconLocation("SoulEnergyBottle"));
+		for(int i = 1; i <= icoFillings.length; i++)
+			icoFillings[i-1] = iconRegister.func_94245_a(CommonProxy.getIconLocation("SoulEnergyBottle_"+i));
 	}
 
 	public static int getSoulEnergyAmount(ItemStack item) {
-		if (item.getItemDamage() == largeFilled)
+		if (getTypeFromDamage(item.getItemDamage()) == largeFilled)
 			return 100000;
 
 		if (!item.hasTagCompound())
@@ -52,6 +75,36 @@ public class SoulEnergyBottle extends Item {
 		}
 	}
 
+	private static int getTypeFromDamage(int damage) {
+		if(damage<=3)
+			return damage;
+		
+		
+		if(damage>=4 && damage<=11)
+			return small;
+		if(damage>=12 && damage<=19)
+			return medium;
+		if(damage>=20 && damage<=27)
+			return large;
+		
+		return small;
+	}
+	
+	private static int getSoulEnergyAmountMax(ItemStack item) {
+		int itemDamage = item.getItemDamage();
+		switch (getTypeFromDamage(item.getItemDamage())) {
+			case small:
+				return 1000;
+			case medium:
+				return 10000;
+			case large:
+				return 100000;
+			case largeFilled:
+				return 100000;
+			default: return 100000;
+		}
+	}
+	
 	public static int addSoulEnergy(int nAmount, ItemStack item) {
 		if (nAmount == 0)
 			return 0;
@@ -66,20 +119,7 @@ public class SoulEnergyBottle extends Item {
 			nExistingAmount = item.getTagCompound().getInteger("SoulEnergyAmountNew");
 		}
 
-		switch (item.getItemDamage()) {
-		case small:
-			nLimit = 1000;
-			break;
-		case medium:
-			nLimit = 10000;
-			break;
-		case large:
-			nLimit = 100000;
-			break;
-		case largeFilled:
-			nLimit = 100000;
-			break;
-		}
+		nLimit = getSoulEnergyAmountMax(item);
 
 		if (nExistingAmount + nAmount > nLimit) {
 			nRest = (nExistingAmount + nAmount) - nLimit;
@@ -89,13 +129,38 @@ public class SoulEnergyBottle extends Item {
 		}
 
 		item.getTagCompound().setInteger("SoulEnergyAmountNew", nExistingAmount);
+		updateItemForScale(item);
 		return nRest;
 	}
 
+	private static int getFillingScale(ItemStack item) {
+		int amount = getSoulEnergyAmount(item);
+		int maxamount = getSoulEnergyAmountMax(item);
+		return (int)Math.floor(((double)amount / (double)maxamount)*(double)8);
+	}
+	
+	private static void updateItemForScale(ItemStack item) {
+		int scale = getFillingScale(item);
+		int type = getTypeFromDamage(item.getItemDamage());
+		if(scale == 0)
+			item.setItemDamage(type);
+		else
+		{
+			switch(type) {
+			case small:
+				item.setItemDamage(scale + 3); break;
+			case medium:
+				item.setItemDamage(scale + 3 + 8); break;
+			case large: 
+				item.setItemDamage(scale + 3 + 8 + 8); break;
+			}
+		}
+	}
+	
 	@Override
 	public String getItemDisplayName(ItemStack is) {
-		if (is.getItemDamage() < itemDisplayNames.length && is.getItemDamage() >= 0)
-			return itemDisplayNames[is.getItemDamage()];
+		if (getTypeFromDamage(is.getItemDamage()) < itemDisplayNames.length && is.getItemDamage() >= 0)
+			return itemDisplayNames[getTypeFromDamage(is.getItemDamage())];
 		else
 			return  itemDisplayNames[0];
 	}
@@ -132,26 +197,12 @@ public class SoulEnergyBottle extends Item {
 		int nExistingAmount = 0;
 		updateOldToNewSoulEnergyTag(par1ItemStack);
 
-		if (par1ItemStack.getItemDamage() == largeFilled) {
+		if (getTypeFromDamage(par1ItemStack.getItemDamage()) == largeFilled) {
 			par3List.add("Contains infinite Soulenergy");
 			return;
 		}
 
-		int nLimit = 0;
-		switch (par1ItemStack.getItemDamage()) {
-		case small:
-			nLimit = 1000;
-			break;
-		case medium:
-			nLimit = 10000;
-			break;
-		case large:
-			nLimit = 100000;
-			break;
-		case largeFilled:
-			nLimit = 100000;
-			break;
-		}
+		int nLimit = getSoulEnergyAmountMax(par1ItemStack);
 		
 		if (par1ItemStack.hasTagCompound() && par1ItemStack.stackTagCompound.hasKey("SoulEnergyAmountNew")) {
 			nExistingAmount = par1ItemStack.getTagCompound().getInteger("SoulEnergyAmountNew");
@@ -175,7 +226,9 @@ public class SoulEnergyBottle extends Item {
 			int nNewAmount = nCurrentAmount - i;
 			if (nNewAmount < 0)
 				nNewAmount = 0;
+			
 			item.getTagCompound().setInteger("SoulEnergyAmountNew", nNewAmount);
+			updateItemForScale(item);
 		}
 	}
 
@@ -185,6 +238,7 @@ public class SoulEnergyBottle extends Item {
 			if (!item.hasTagCompound())
 				item.stackTagCompound = new NBTTagCompound();
 			item.getTagCompound().setInteger("SoulEnergyAmountNew", i);
+			updateItemForScale(item);
 		}
 	}
 }
